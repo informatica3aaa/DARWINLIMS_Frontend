179b6750f0b702aa8daed68ccaf8974144dd8f11
unknown <=>
2023-06-29 00:46:34 -0400

    get requicisiones

--- src/store/modules/requisicion.js
+++ src/store/modules/requisicion.js
@@ -1,13 +1,39 @@
 
 import axios from 'axios'
 const state = {
-   
+   requisiciones : []
 }
 
 const mutations = {  
+    SET_REQUISICIONES(state, payload){  state.requisiciones = payload },
+
 }
 
 const actions = {  
+
+    async getRequicisiones({commit}, payload)
+    {
+        let loading = payload.loading.show()
+
+        try {
+
+            
+            if(!payload.offset) throw { message: 'falta el campo : offset'} 
+            if(!payload.limit) throw { message: 'falta el campo : limit'} 
+
+            const { data } =  await axios.post('/api/requisition/list', payload)
+
+            if(!data.ok) throw { message: 'No se logro consultar por las requisiciones'} 
+
+            await commit('SET_REQUISICIONES', data.data)
+
+            loading.hide()
+        } catch (error) {
+            payload.toast.error("Error No se logro consultar por las requisiciones")
+            loading.hide()
+            console.error('Error No se logro consultar por las requisiciones: ', error) 
+        } 
+    },
 
     async grabar(state, payload)
     {
